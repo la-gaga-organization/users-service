e9f1a2e09944e34f067a3670640eb6751bc40a5d
Federico Zorgnotto <fede.zorgnotto@gmail.com>
2025-11-19 13:22:18 +0100

    update: aggiornato test per rimozione username

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,5 @@
 def test_create_user_success(client):
     payload = {
-        "username": "admin",
         "hashed_password": "admin",
         "email": "gaga@gaga.com",
         "name": "gaga",
@@ -10,6 +9,5 @@ def test_create_user_success(client):
 
     assert response.status_code == 201
     data = response.json()
-    assert data["username"] == payload["username"]
     assert data["email"] == payload["email"]
     assert "id" in data
