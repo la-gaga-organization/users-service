48694da7880e2e4982f676d100af5ba73ef9f26d
Federico Zorgnotto <fede.zorgnotto@gmail.com>
2025-12-04 11:10:11 +0100

    fix: modificati endpoint verifica mail per lavorare con body, ref orientati/Orientati-Backend#39

--- app/api/v1/routes/users.py
+++ app/api/v1/routes/users.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 from typing import List
 
 from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Body
 from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
 from sqlalchemy.orm import Session
 
 from app.api.deps import get_db
@@ -141,10 +143,13 @@ async def api_delete_user(user_id: int, db: Session = Depends(get_db)):
 
 
 @router.post("/request_email_verification", status_code=status.HTTP_204_NO_CONTENT)
-async def api_request_email_verification(user_id: int, db: Session = Depends(get_db)):
+async def api_request_email_verification(
+        user_id: int = Body(..., embed=True),
+        db: Session = Depends(get_db)
+):
     await request_email_verification(user_id, db)
 
 
 @router.post("/verify_email", status_code=status.HTTP_204_NO_CONTENT)
-async def api_verify_email(token: str, db: Session = Depends(get_db)):
+async def api_verify_email(token: str = Body(..., embed=True)):
     await verify_email(token)
